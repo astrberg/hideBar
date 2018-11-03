79c7e8084d4546bf8801973a2382a1664cb0ecc7
Aron Strandberg <arostr-5@student.ltu.se>
2018-11-03 01:56:47 +0100

    SetAlpha(0) not hiding, problem with consistent hiding of spells/action. Trying to resolve with hooks

--- hide.lua
+++ hide.lua
@@ -1,48 +1,57 @@
--- init
-local Frame = CreateFrame("Frame")
-Frame:RegisterEvent("ADDON_LOADED")
+-- Author: Aron Strandberg
 
+--init
+local Hide = CreateFrame("Frame")
+function Hide_EventHandler(self, event)
+-- This solved issues with api calls if player not fully loaded
+    if(event == "PLAYER_ENTERING_WORLD") then
+        Hide_ActionButton()
+        Hide_Bar()
+        Hide_Bling()
 
-function Frame:OnEvent(event, ...)
-
-    if event == "ADDON_LOADED" then
-        local addon = ...
-    if addon == "hideBar" then
- 
-    -- Hide specific frames and buttons
-    MainMenuBarArtFrameBackground:Hide()
-    MainMenuBarArtFrame.LeftEndCap:Hide()
-    MainMenuBarArtFrame.RightEndCap:Hide()
-    ActionBarUpButton:Hide()
-    ActionBarDownButton:Hide()
-    StatusTrackingBarManager:Hide()
-    MainMenuBarArtFrame.PageNumber:Hide()
-
-   -- Hide actionbuttons
-    for i = 1,12 do 
-        _G["ActionButton"..i]:SetAlpha(0)
-    
     end
+end
 
-    -- Hide GCD Bling, thanks to https://wow.curseforge.com/projects/cooldownreadyflashdisabler
+
+function Hide_Bar()
+    -- Hide specific frames and buttons
+        MainMenuBarArtFrameBackground:Hide()
+        MainMenuBarArtFrame.LeftEndCap:Hide()
+        MainMenuBarArtFrame.RightEndCap:Hide()
+        ActionBarUpButton:Hide()
+        ActionBarDownButton:Hide()
+        StatusTrackingBarManager:Hide()
+        MainMenuBarArtFrame.PageNumber:Hide()
+end
+
+function Hide_ActionButton()
+-- Hide actionbuttons
+    for i = 1,12 do 
+        _G["ActionButton".. i]:Hide()
+        --_G["ActionButton" .. i]:UnregisterAllEvents()
+        
+        
+
+    end
+end
+
+function Hide_Bling()
+-- Hide GCD Bling, thanks to https://wow.curseforge.com/projects/cooldownreadyflashdisabler
     for k,v in pairs(_G) do
         if type(v)=="table" and type(v.SetDrawBling)=="function" then
             v:SetDrawBling(false)
-        
+
         end
     end
     hooksecurefunc(getmetatable(ActionButton1Cooldown).__index, 'SetCooldown', function(self)
-        self:SetDrawBling(false)
+    self:SetDrawBling(false)
     end)
-    
-    
-end
-end
 end
 
 
--- Run
-Frame:SetScript("OnEvent", Frame.OnEvent)
+--run
+Hide:SetScript("OnEvent", Hide_EventHandler)
+Hide:RegisterEvent("PLAYER_ENTERING_WORLD")
 
 
 
